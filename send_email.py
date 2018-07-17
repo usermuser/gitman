da43620d5fcbce9029eac6b42b2c9eb587968fb0
usermuser <lwrdrstv@gmail.com>
2018-07-17 23:07:09 +0500

    send_email_working

--- send_email.py
+++ send_email.py
@@ -9,30 +9,24 @@ from_email = "api.myip.com@gmail.com"
 to_email = "xakacb4@mail.ru"
 
 
-def send_email ( url, login=login, psw=psw, from_email=from_email):
-    text = get_ip(url)
+r = requests.get(url)
+if r.status_code == 200:
+    j = r.json()
+    text = j.get('ip')
+    print('ip =', text)
+
     smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
     smtpObj.starttls()
     smtpObj.login(login, psw)
     smtpObj.sendmail(from_email, to_email, text)
-
-def get_ip(url):
-    try:
-        r = requests.get(url)
-        if r.status_code == 200:
-            j = r.json()
-            ip = j.get('ip')
-            send_email(url)
-            print('', ip)
-        else:
-            print('Error occured, server status_code =', r.status_code)
-            text = r.status_code
-            send_email()
-    except Exception as e:
-        print('Error occured!!', e)
+    print('email sended =', text)
+        
+else:
+    print('Error occured, server status_code =', r.status_code)
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpObj.starttls()
+    smtpObj.login(login, psw)
+    smtpObj.sendmail(from_email, to_email, r.status_code)
+    
 
 
-
-
-text = get_ip(url)
-get_ip(url=url)
